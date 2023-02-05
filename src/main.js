f144b69d321c38d43742cdde64563be3ce76ff36
manjinder <manjinder@xyrintech.com>
2023-02-05 18:22:52 +0530

    Page of Simpletricks

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
 
-createApp(App).mount('#app')
+import ButTon from "primevue/button";
+import ColUmn from "primevue/column";
+import DataTable from "primevue/datatable";
+import CaleNdar from "primevue/calendar";
+import ColumnGroup from "primevue/columngroup"; //optional for column grouping
+import TableRow from "primevue/row";
+import CaRd from "primevue/card";
+import DropDown from "primevue/dropdown";
+
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.component("ButTon", ButTon);
+app.component("ColUmn", ColUmn);
+app.component("CaleNdar", CaleNdar);
+app.component("DropDown", DropDown);
+app.component("CaRd", CaRd);
+app.component("ColumnGroup", ColumnGroup);
+app.component("TableRow", TableRow);
+app.component("DataTable", DataTable);
+
+app.mount("#app");
